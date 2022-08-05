f9877539781670d9436b68e8a554908395afcf64
yassine-rd <yassine.rodani@gmail.com>
2022-08-05 16:37:16 +0100

    updating js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,25 @@
-/*==================== MENU SHOW & HIDDEN ====================*/
+/*==================== SHOWING & HIDING MENU ====================*/
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
   navClose = document.getElementById("nav-close");
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
+/*===== SHOW MENU =====*/
+/* Checking if constant exists */
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
   });
 }
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
+/*===== HIDE MENU =====*/
+/* Checking if constant exists */
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
   });
 }
 
-/*==================== REMOVE MENU MOBILE ====================*/
+/*==================== REMOVING MOBILE MENU ====================*/
 const navLink = document.querySelectorAll(".nav__link");
 
 function linkAction() {
@@ -29,7 +29,7 @@ function linkAction() {
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-/*==================== ACCORDION SKILLS ====================*/
+/*==================== SHOWING SKILLS ====================*/
 const skillsContent = document.getElementsByClassName("skills__content"),
       skillsHeader = document.querySelectorAll(".skills__header");
 
@@ -68,7 +68,7 @@ tabs.forEach(tab =>{
     })
 })
 
-/*==================== PROJECTS SWIPER  ====================*/
+/*==================== PROJECTS SWIPER ====================*/
 let swiper = new Swiper('.projects__container', {
     cssMode: true,
     loop:true,
@@ -83,7 +83,7 @@ let swiper = new Swiper('.projects__container', {
     },
 });
 
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+/*==================== SCROLLING THROUGH SECTIONS & ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
@@ -103,7 +103,7 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
-/*==================== CHANGE BACKGROUND HEADER ====================*/
+/*==================== CHANGING HEADER BACKGROUND ====================*/
 function scrollHeader(){
     const nav = document.getElementById('header')
     // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
@@ -111,7 +111,7 @@ function scrollHeader(){
 }
 window.addEventListener('scroll', scrollHeader)
 
-/*==================== SHOW SCROLL UP ====================*/
+/*==================== SHOWING SCROLL UP BUTTON ====================*/
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
@@ -119,12 +119,12 @@ function scrollUp(){
 }
 window.addEventListener('scroll', scrollUp)
 
-/*==================== DARK LIGHT THEME ====================*/
+/*==================== DARK/LIGHT THEME ====================*/
 const themeButton = document.getElementById('theme-button')
 const darkTheme = 'dark-theme'
 const iconTheme = 'uil-sun'
 
-// Previously selected topic (if user selected)
+// Previously selected theme
 const selectedTheme = localStorage.getItem('selected-theme')
 const selectedIcon = localStorage.getItem('selected-icon')
 
@@ -132,9 +132,8 @@ const selectedIcon = localStorage.getItem('selected-icon')
 const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
 const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
 
-// We validate if the user previously chose a topic
+// We validate if the user previously chose a theme
 if (selectedTheme) {
-  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
   document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
   themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
 }
@@ -144,7 +143,7 @@ themeButton.addEventListener('click', () => {
     // Add or remove the dark / icon theme
     document.body.classList.toggle(darkTheme)
     themeButton.classList.toggle(iconTheme)
-    // We save the theme and the current icon that the user chose
+    // We save the theme and the current icon that the user has chosen
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
